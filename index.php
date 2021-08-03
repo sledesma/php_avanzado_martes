f39138d46c174fea035f63e263c86a0f0cd791c8
sledesma <sebas@Scorpion.sebas.com>
2021-08-03 20:50:44 -0300

    Primera parte

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
 /**
- * 
- */
+ * Recibir la PETICION ACTUAL
+**/
+
+define('URI_PREFIJO', '/php_avanzado_martes');
+
+$peticionActual = [
+  'uri' => $_SERVER['REQUEST_URI'],
+  'metodo' => '',
+  'cuerpo' => '',
+  'cabeceras' => ''
+];
+
+switch($peticionActual['uri']) {
+
+  case URI_PREFIJO.'/':
+    echo 'Hola mundo desde home';
+    break;
+
+  case URI_PREFIJO.'/contacto':
+    echo 'Estas en contacto';
+
+}
+
+
+/**
+ * Enviar una RESPUESTA
+**/
